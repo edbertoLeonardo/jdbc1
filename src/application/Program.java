66d6230c52fcad73e0a830d2dc41a7c36f49bdf3
Edberto Leonardo <edbleonardo@gmail.com>
2020-02-05 11:39:59 -0300

    jdbc2-Recuperar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,43 @@
 package application;
 
 import java.sql.Connection;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import db.DB;
 
 public class Program {
+
 	public static void main(String[] args) {
-		
-		Connection connection = DB.getConnection();
-		DB.closeConnection();
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultset = null;
+
+		try {
+			connection = DB.getConnection();
+			statement = connection.createStatement();
+			resultset = statement.executeQuery("select * from department");
+			while (resultset.next()) {
+				System.out.println(resultset.getInt("Id") + ", " + resultset.getString("Name"));
+			}
+
+		}
+
+		catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+		finally {
+
+			DB.closeResultSet(resultset); 
+			DB.closeStatement(statement);
+			DB.closeConnection();
+
+		}
+
 	}
 
 }
